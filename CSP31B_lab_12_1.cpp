8f613fb0a3a9d125694912d85535a09eb7d968b0
XxOATSxX <166959223+XxOATSxX@users.noreply.github.com>
2024-11-17 23:53:52 -0800

    comment

---
+++ CSP31B_lab_12_1.cpp
@@ -0,0 +1,12 @@
+/*
+Create an array with these numbers: 2.4, 1.3, 2.4, 4.5, 6.7,
+4.5, 9.0
+Use the find function to find the position of 4.5 in the array.
+Use the find_if function to find the position of the first element that
+is less than 2.
+Use the find_end function to find the sequence position {2.4, 4.5} in
+the array.
+Use the find_first_of function to find the position of the first
+common element in the array and a list with these values
+{34,55,2.4,4.5}
+*/
